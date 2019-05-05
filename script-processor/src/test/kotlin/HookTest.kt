3df155df03faf0cca4f80e11a23c06023740e3d4
Christof Hullaert <christof.hullaert@cover3d.com>
2019-05-05 18:56:33 +0200

    refactoring: use of parametrized test of junit

--- script-processor/src/test/kotlin/HookTest.kt
+++ script-processor/src/test/kotlin/HookTest.kt
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class HookTest {
 
@@ -7,20 +9,14 @@ class HookTest {
         return input.replace(" ", "").replace("\n", "")
     }
 
-    @Test
-    fun whenHookThenUpdate() {
+    @ParameterizedTest
+    @CsvSource(value=["/hook-before.kt,/hook-after.kt", "/hook-before-2.kt,/hook-after-2.kt"])
+    fun whenHookThenUpdate(input: String, expected: String) {
         val scriptHooks= mutableListOf(ScriptHook("container", "add", patterns = arrayOf("person")))
-        val before=ScriptRunner::class.java.getResource("/hook-before.kt").readText()
-        val after=ScriptRunner::class.java.getResource("/hook-after.kt").readText()
+        val before=ScriptRunner::class.java.getResource(input).readText()
+        val after=ScriptRunner::class.java.getResource(expected).readText()
         assertEquals(clearFile(after), clearFile(ScriptHookModifier(scriptHooks).update(before)))
 
     }
-    @Test
-    fun whenHookMultiThenUpdate() {
-        val scriptHooks= mutableListOf(ScriptHook("container", "add", patterns = arrayOf("person")))
-        val before=ScriptRunner::class.java.getResource("/hook-before-2.kt").readText()
-        val after=ScriptRunner::class.java.getResource("/hook-after-2.kt").readText()
-        assertEquals(clearFile(after), clearFile(ScriptHookModifier(scriptHooks).update(before)))
 
-    }
 }

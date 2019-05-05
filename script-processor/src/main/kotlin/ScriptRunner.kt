f4c77d64d343f921da15f01c93099bd410600701
Christof Hullaert <christof.hullaert@cover3d.com>
2019-05-05 21:41:17 +0200

    add script bindings

--- script-processor/src/main/kotlin/ScriptRunner.kt
+++ script-processor/src/main/kotlin/ScriptRunner.kt
@@ -1,8 +1,18 @@
 import javax.script.ScriptEngine
 import javax.script.ScriptEngineManager
+import javax.script.SimpleBindings
 
 class ScriptRunner(private val scriptFile: String?=null,
-                   private val scriptHooks: List<ScriptHook>) {
+                   private val scriptHooks: List<ScriptHook>,
+                   private val options: Set<Option>) {
+
+    class Variable(val name: String,
+                   val type: String,
+                   val value: Any)
+
+    enum class Option {
+        GENERATE_MAIN
+    }
 
     private val engine: ScriptEngine by lazy {
         (ScriptEngineManager().getEngineByExtension("kts"))
@@ -14,9 +24,16 @@ class ScriptRunner(private val scriptFile: String?=null,
         else ""
     }
 
-    fun load() {
-        println("script: \n%s".format(script))
-        //this.engine.eval(script)
+    fun run(variables: List<Variable> = emptyList()) {
+        val bindings=this.engine.createBindings()
+        variables.forEach {
+            bindings[it.name]=it.value
+        }
+
+        val bindingCode = variables.map { "val %s = bindings[\"%s\"] as %s".format(it.name, it.name, it.type)  }
+                                   .joinToString("\n")
+        val code = script.replace("###BINDINGS###", "%s\n".format(bindingCode))
+        this.engine.eval(code, bindings)
     }
 
     private fun clean(script: String): String {
@@ -27,7 +44,23 @@ class ScriptRunner(private val scriptFile: String?=null,
     }
 
     private fun prepare(script: String): String {
-        return "import com.darwinit.annotation.demo.*\n\n" + clean(ScriptHookModifier(this.scriptHooks).update(script))
+        var current = script
+
+        current = clean(current)
+        if(options.contains(Option.GENERATE_MAIN)) {
+            current = "fun main() {%s}".format(current)
+        }
+
+        current = ScriptHookModifier(this.scriptHooks).update(current)
+
+        if(options.contains(Option.GENERATE_MAIN)) {
+            current = current.split("\n")
+                                .drop(1)
+                                .dropLast(2)
+                                .joinToString("\n")
+        }
+
+        return "import com.darwinit.annotation.demo.*\n\n###BINDINGS###%s".format(current)
     }
 
 }
--- src/test/kotlin/PersonDslTest.kt
+++ src/test/kotlin/PersonDslTest.kt
@@ -1,11 +1,15 @@
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 
 class PersonDslTest {
 
     @Test
     fun testSimplePerson() {
-        val scriptHooks= mutableListOf(ScriptHook("container.add", patterns = arrayOf("person {")))
-        val runner=ScriptRunner("/scripts/SimplePerson.ylib", scriptHooks)
-        runner.load()
+        val scriptHooks= mutableListOf(ScriptHook("container", "add", patterns = arrayOf("person")))
+        val runner=ScriptRunner("/scripts/SimplePerson.ylib", scriptHooks, setOf(ScriptRunner.Option.GENERATE_MAIN))
+        val container = mutableListOf<Any>()
+        runner.run(listOf(ScriptRunner.Variable("container", "MutableList<Any>", container)))
+        assertThat(container.size).isEqualTo(1)
     }
 }
